17e09a411d98a5a033a367151def0254ef2c0ce8
mbrenesn <mbrenes@ictp.it>
2019-04-24 11:55:22 +0100

    Stark loicalisation driver

--- src/Driver/driver.cc
+++ src/Driver/driver.cc
@@ -1,3 +1,5 @@
+// Stark localisation
+
 #include <iostream>
 #include <sys/time.h>
 
@@ -5,7 +7,7 @@
 #include "mkl_lapacke.h"
 
 #include "../Basis/Basis.h"
-#include "../Hamiltonian/XXZ.h"
+#include "../Hamiltonian/StarkM.h"
 
 double seconds()
 {
@@ -20,14 +22,14 @@ int main(int argc, char **argv)
 {
   MKL_INT l = 777;
   MKL_INT n = 777;
-  double alpha_val = 0.777;
-  double delta_val = 0.777;
-  double h_val = 0.777;
-  bool periodic = true;
+  double J = 0.777;
+  double V = 0.777;
+  double gamma = 0.777;
+  double alpha = 0.777;
 
   if(argc != 13){
     std::cerr << "Usage: " << argv[0] << 
-      " --l [sites] --n [fill] --alpha [alpha] --delta [delta] --h [h] --periodic [bool]" 
+      " --l [sites] --n [fill] --J [J] --V [V] --gamma [gamma] --alpha [alpha]" 
         << std::endl;
     exit(1);
   }
@@ -36,62 +38,46 @@ int main(int argc, char **argv)
     std::string str = argv[i];
     if(str == "--l") l = atoi(argv[i + 1]);
     else if(str == "--n") n = atoi(argv[i + 1]);
-    else if(str == "--alpha") alpha_val = atof(argv[i + 1]);
-    else if(str == "--delta") delta_val = atof(argv[i + 1]);
-    else if(str == "--h") h_val = atof(argv[i + 1]);
-    else if(str == "--periodic") periodic = atoi(argv[i + 1]);
+    else if(str == "--J") J = atof(argv[i + 1]);
+    else if(str == "--V") V = atof(argv[i + 1]);
+    else if(str == "--gamma") gamma = atof(argv[i + 1]);
+    else if(str == "--alpha") alpha = atof(argv[i + 1]);
     else continue;
   }
 
-  if(l == 777 || n == 777 || alpha_val == 0.777 || delta_val == 0.777 || h_val == 0.777){
+  if(l == 777 || n == 777 || J == 0.777 || V == 0.777 || gamma == 0.777 || alpha == 0.777){
     std::cerr << "Error setting parameters" << std::endl;
     std::cerr << "Usage: " << argv[0] << 
-      " --l [sites] --n [fill] --alpha [alpha] --delta [delta] --h [h] --periodic [bool]" 
+      " --l [sites] --n [fill] --J [J] --V [V] --gamma [gamma] --alpha [alpha]" 
         << std::endl;
     exit(1);
   }
 
-  std::vector<double> alpha(l, alpha_val);
-  std::vector<double> delta(l, delta_val);
-  std::vector<double> h(l, 0.0);
-  // Impurity model
-  h[l / 2] = h_val;
-
   std::cout << std::fixed;
-  std::cout.precision(1);
+  std::cout.precision(3);
+#if 0  
   std::cout << "# Parameters:" << std::endl;
   std::cout << "# L = " << l << std::endl;
   std::cout << "# N = " << n << std::endl;
-  std::cout << "# Periodic boundaries: " << periodic << std::endl;
-  std::cout << "# Alpha = " << "[";
-  for(int i = 0; i < (l - 1); ++i){
-    std::cout << alpha[i] << ", ";
-  }
-  std::cout << alpha[l - 1] << "]" << std::endl;
-  std::cout << "# Delta = " << "[";
-  for(int i = 0; i < (l - 1); ++i){
-    std::cout << delta[i] << ", ";
-  }
-  std::cout << delta[l - 1] << "]" << std::endl;
-  std::cout << "# h = " << "[";
-  for(int i = 0; i < (l - 1); ++i){
-    std::cout << h[i] << ", ";
-  }
-  std::cout << h[l - 1] << "]" << std::endl;
+  std::cout << "# J = " << J << std::endl;
+  std::cout << "# V = " << V << std::endl;
+  std::cout << "# gamma = " << gamma << std::endl;
+  std::cout << "# alpha = " << alpha << std::endl;
+#endif 
 
   Basis *basis = new Basis(l, n);
   //basis->print_basis();
 
   MKL_INT basis_size = basis->basis_size;
 
-  XXZ heisen( *basis, 
-              periodic, 
-              true );
-  heisen.construct_xxz( basis->int_basis, 
-                        alpha, 
-                        delta, 
-                        h);
-  //heisen.print_ham_mat();
+  StarkM stark( *basis, 
+                false );
+  stark.construct_starkm( basis->int_basis, 
+                          J,
+                          V,
+                          gamma,
+                          alpha);
+  //stark.print_ham_mat();
 
   delete basis;
 
@@ -102,13 +88,13 @@ int main(int argc, char **argv)
   std::vector<double> eigvals(basis_size, 0.0);
   MKL_INT info;
 
-  std::cout << "# Calculating eigen..." << std::endl;
+  //std::cout << "# Calculating eigen..." << std::endl;
   double tic = seconds();
   info = LAPACKE_dsyevd( LAPACK_ROW_MAJOR,
                          'V', 
                          'U',
                          basis_size, 
-                         &heisen.HamMat[0], 
+                         &stark.HamMat[0], 
                          basis_size, 
                          &eigvals[0] );
   if(info > 0){
@@ -116,33 +102,19 @@ int main(int argc, char **argv)
     exit(1);
   }
   double toc = seconds();
-  std::cout << "# Time eigen: " << (toc - tic) << std::endl;
 
-  // Compute expectation value of SigmaZ[l / 2]
-  // Transpose eigvectors
-  mkl_dimatcopy( 'R',
-                 'T',
-                 basis_size,
-                 basis_size,
-                 1,
-                 &heisen.HamMat[0],
-                 basis_size,
-                 basis_size);
-
-  std::vector<double> tmp(basis_size, 0.0);
-  std::vector<double> mags(basis_size, 0.0);
   for(MKL_INT i = 0; i < basis_size; ++i){
-    vdMul(basis_size,
-          &heisen.HamMat[(i * basis_size)],
-          &heisen.SigmaZ[l / 2][0],
-          &tmp[0]);
-    double val = cblas_ddot( basis_size,
-                             &heisen.HamMat[(i * basis_size)],
-                             1,
-                             &tmp[0],
-                             1);
-    std::cout << (eigvals[i] / l) << " " << val << std::endl;
+    double ent = 0.0;
+    for(MKL_INT j = 0; j < basis_size; ++j){
+      double pi = std::pow(stark.HamMat[(j * basis_size) + i], 2.0);
+      ent += -1.0 * pi * std::log(pi);
+    }
+    std::cout << alpha << " " 
+      << (eigvals[i] - eigvals[basis_size - 1]) / (eigvals[0] - eigvals[basis_size - 1]) 
+        << " " << ent << std::endl;
   }
+  
+  //std::cout << "# Time eigen: " << (toc - tic) << std::endl;
 
   return 0;
 }
